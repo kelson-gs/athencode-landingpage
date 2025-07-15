15bc457aea9bbab6e3c012e036995b03fc17c428
kelson Gonçalves <kelsonredbana12@gmail.com>
2025-07-15 15:25:19 -0300

    adiciona build na pasta out

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true, // opcional mas recomendado pra hospedagem estática
+};
 export default nextConfig;
